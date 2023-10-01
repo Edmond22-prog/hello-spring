ef4ab91be4bbce6399fb116f9ecbdf5504b01aba
Edghi <edghimakoll@gmail.com>
2023-10-01 23:08:39 +0100

    New implementation for making a Hello Spring Boot

--- src/main/java/com/example/hellospring/HellospringApplication.java
+++ src/main/java/com/example/hellospring/HellospringApplication.java
@@ -1,19 +1,13 @@
 package com.example.hellospring;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HellospringApplication implements CommandLineRunner {
+public class HellospringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HellospringApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		System.out.println("Hello Spring Boot!");
 	}
 
 }
