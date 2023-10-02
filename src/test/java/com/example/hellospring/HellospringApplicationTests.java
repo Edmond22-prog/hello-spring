39011a470dd7044607cb7bcf47d12adede4cfb17
Edghi <edghimakoll@gmail.com>
2023-10-02 15:21:36 +0100

    test:first unit test in Springboot

--- src/test/java/com/example/hellospring/HellospringApplicationTests.java
+++ src/test/java/com/example/hellospring/HellospringApplicationTests.java
@@ -1,13 +1,29 @@
 package com.example.hellospring;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.hellospring.model.HelloSpringBoot;
+import com.example.hellospring.service.BusinessService;
 
 @SpringBootTest
 class HellospringApplicationTests {
+	@Autowired
+	private BusinessService businessService;
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void testGetHelloSprintBoot() {
+		String expectedResult = "Hello Spring Boot !";
+
+		HelloSpringBoot helloSpringBoot = businessService.getHelloSprintBoot();
+		assertEquals(helloSpringBoot.toString(), expectedResult);
 	}
 
 }
